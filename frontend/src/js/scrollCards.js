e3944166c178c989a32993086622dcbfe030ed09
Vadym <67538058+Extractoro@users.noreply.github.com>
2024-11-26 18:11:22 +0200

    add: tracks scroll

--- frontend/src/js/scrollCards.js
+++ frontend/src/js/scrollCards.js
@@ -1,51 +1,10 @@
 const container = document.querySelector('.main-tracks--container-wrapper');
 
-let isDragging = false;
-let startX;
-let scrollLeft;
-
-container.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-    container.style.cursor = 'grabbing';
-});
-
-container.addEventListener('mouseup', (e) => {
-    isDragging = false;
-    container.style.cursor = 'default';
-    const deltaX = Math.abs(e.pageX - startX);
-    if (deltaX > 5) {
-        e.preventDefault(); // Предотвращает переход по ссылке, если был скролл.
+const handleWheelScroll = (event) => {
+    if (window.innerWidth >= 1024) {
+        event.preventDefault();
+        container.scrollLeft += event.deltaY;
     }
-});
+};
 
-container.addEventListener('mouseleave', () => {
-    isDragging = false;
-    container.style.cursor = 'default';
-});
-
-container.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    e.preventDefault();
-    const x = e.pageX - container.offsetLeft;
-    const walk = x - startX;
-    container.scrollLeft = scrollLeft - walk;
-});
-
-container.addEventListener('touchstart', (e) => {
-    isDragging = true;
-    startX = e.touches[0].pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-});
-
-container.addEventListener('touchend', () => {
-    isDragging = false;
-});
-
-container.addEventListener('touchmove', (e) => {
-    if (!isDragging) return;
-    const x = e.touches[0].pageX - container.offsetLeft;
-    const walk = (x - startX) * 2;
-    container.scrollLeft = scrollLeft - walk;
-});
+container.addEventListener('wheel', handleWheelScroll);
